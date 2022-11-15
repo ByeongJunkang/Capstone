3e2c82eaec0513ef889e8822263801347f09a8c1
강병준 <freudbjk@gmail.com>
2022-11-15 14:47:57 +0900

    회원가입 api 수정

--- common/views.py
+++ common/views.py
@@ -1,28 +1,42 @@
 from django.contrib.auth import authenticate, login
-from rest_framework.decorators import api_view
-from common.forms import RegisterForm
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import User
-from .forms import CustomCsUserChangeForm
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.permissions import AllowAny
-from rest_framework.authtoken.models import Token
+from django.shortcuts import get_object_or_404, redirect, render
 from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from common.forms import RegisterForm
+
+from .forms import CustomCsUserChangeForm
+from .models import User
 from .serializers import UserSerializer
 
 
-class UserView(APIView):
+
+class RegistrationAPIView(APIView):
+    permission_classes(AllowAny)
     def post(self, request):
+
         user_serializer = UserSerializer(data=request.data) #Request의 data를 UserSerializer로 변환
         if user_serializer.is_valid():
             user_serializer.save() #UserSerializer의 유효성 검사를 한 뒤 DB에 저장
-            return Response(user_serializer.data, status=status.HTTP_201_CREATED) #client에게 JSON response 전달
+            return Response(user_serializer.data, status=status.HTTP_201_CREATED) 
+        
         else:
             return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# Create your views here.
+class UserView(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 #홈페이지 회원가입
